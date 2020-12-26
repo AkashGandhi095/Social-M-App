85347e56b9127f79233fb4202e5c1eab52586206
Akash Gandhi <47486836+AkashGandhi095@users.noreply.github.com>
2020-12-26 11:31:54 +0530

    asus initial commit

--- app/src/main/java/com/app/social_m/fragments/UsersFragment.kt
+++ app/src/main/java/com/app/social_m/fragments/UsersFragment.kt
@@ -1,14 +1,24 @@
 package com.app.social_m.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.app.social_m.R
+import com.app.social_m.model.Users
+import com.app.social_m.retrofitUtils.RetrofitService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class UsersFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "UsersFragmentScreen"
+    }
 
 
     override fun onCreateView(
@@ -22,10 +32,27 @@ class UsersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        fetchUsersList()
     }
 
+
+    private fun fetchUsersList() {
+
+        RetrofitService.apiService.fetchUsers().enqueue(object : Callback<List<Users>> {
+            override fun onResponse(call: Call<List<Users>>, response: Response<List<Users>>) {
+                if (response.isSuccessful) {
+
+                } else {
+                    Log.d(TAG, "onResponse: errorResponse :${response.errorBody()} ")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Users>>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.localizedMessage}")
+            }
+
+        })
+    }
 
 
 }
--- app/src/main/java/com/app/social_m/retrofitUtils/RetrofitService.kt
+++ app/src/main/java/com/app/social_m/retrofitUtils/RetrofitService.kt
@@ -5,34 +5,36 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.log
 
 object RetrofitService {
 
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
     private const val TAG = "RetrofitServiceClass"
 
-    /*private val httpClientBuilder :OkHttpClient.Builder by lazy {
-        OkHttpClient.Builder()
-    }
-
     private val httpLogger :HttpLoggingInterceptor by lazy {
-        Log.d(TAG, "HttpClientBuilder hashcode: ${httpClientBuilder.hashCode()}")
         HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
-            httpClientBuilder.addNetworkInterceptor(this)
-            httpClientBuilder.build()
         }
-    }*/
+    }
 
+    private val httpClientBuilder :OkHttpClient.Builder by lazy {
+        Log.d(TAG, "HttpLoggerBuilder : ${httpLogger.hashCode()}")
+        OkHttpClient.Builder()
+                .addInterceptor(httpLogger)
+    }
 
-
-   /* private val retrofit :Retrofit by lazy {
-       // Log.d(TAG, "HttpLoggingInterceptor: ${httpLogger.hashCode()}")
+    private val retrofit :Retrofit by lazy {
+        Log.d(TAG, "HttpClientBuilder: ${httpClientBuilder.build().hashCode()}")
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client()
+            .client(httpClientBuilder.build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
 
-    }*/
+    val apiService :ApiService by lazy {
+        Log.d(TAG, "retrofit : ${retrofit.hashCode()}")
+        retrofit.create(ApiService::class.java)
+    }
 }
